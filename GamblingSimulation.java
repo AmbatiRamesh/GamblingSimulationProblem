ebfbfca026350c62a7ed58720819f48f666ac6f9
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-07 14:28:51 +0530

    Added: Start with 00 and bet every game

---
+++ GamblingSimulation.java
@@ -0,0 +1,9 @@
+package com.gambling;
+import java.util.*;
+public class GamblingSimulation {
+        public static final int INITIAL_BET_STAKE = 100;
+        public static final int BET_AMT = 1;
+        public static void main(String[] args) {
+
+        }
+}
